6c735a517b08c6c23fcaa6e210326eda7280c7b1
rmangra <richard.mangra@gmail.com>
2018-02-08 00:02:00 -0500

    Windows Forms desktop application that displays scraped data using DataGridView.

---
+++ WSDatagridView/WSDatagridView/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSDatagridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetCustomerList();
+        }
+        private DataTable GetCustomerList()
+        {
+            var dtCustomers = new DataTable();
+            string connString = ConfigurationManager.ConnectionStrings["NWDataGridView.Properties.Settings.NorthwindConnectionString"].ConnectionString;
+            using (var con = new SqlConnection(connString))
+            {
+
+                using (var cmd = new SqlCommand("SELECT * FROM CryptoCurrencies", con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    dtCustomers.Load(reader);
+
+                }
+
+                return dtCustomers;
+            }
+        }
+    }
+}
